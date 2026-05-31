a62195c94dd5a9afb7e75b9d864bd1e770886809
Manojkumar T <heymanojt@gmail.com>
2026-05-31 18:46:34 +0530

    Add files via upload

---
+++ Day-2/For_loop/E&O_Count.py
@@ -0,0 +1,9 @@
+e_count = 0
+o_count = 0
+for i in range (1,11):
+    if(i%2==0):
+        e_count = e_count+1
+    else:
+        o_count = o_count+1
+print("Even no :",e_count)
+print("Odd no :",o_count) 
---
+++ Day-2/For_loop/For_Loop_2Tables.py
@@ -0,0 +1,2 @@
+for i in range (1,11):
+    print(i,"x 2 =",i*2)
