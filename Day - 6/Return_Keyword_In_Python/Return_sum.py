6db5971fb89a6273b8eb93c4a52dd6e3ef562d62
Manojkumar T <heymanojt@gmail.com>
2026-06-05 17:10:46 +0530

    Add files via upload

---
+++ Day - 6/Return_Keyword_In_Python/Return_sum.py
@@ -0,0 +1,13 @@
+def add(n1,n2):
+    return n1+n2
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+added = add(a,b)
+Output = added*c
+
+print(Output)
+  
+ 
---
+++ Day - 6/Return_Keyword_In_Python/Validate.py
@@ -0,0 +1,15 @@
+s_username = "Manoj"
+s_password = "123"
+
+uname = input()
+password = input()
+
+def validate():
+    if(s_username == uname and s_password == password):
+        return True
+    else:
+        return False
+
+a = validate()
+print(a)
+  
